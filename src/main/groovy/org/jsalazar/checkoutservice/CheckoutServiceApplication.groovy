0c0577b5e0730caa2bfb712ccb2874e23eaaf0da
jsalazare <javi_9315@hotmail.com>
2020-09-23 16:45:21 -0700

    init project

---
+++ src/main/groovy/org/jsalazar/checkoutservice/CheckoutServiceApplication.groovy
@@ -0,0 +1,13 @@
+package org.jsalazar.checkoutservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CheckoutServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CheckoutServiceApplication, args)
+    }
+
+}
